8ea597079dd3d592eea988b3ebd17e77a1bf595a
gen0v <eugen.b95@googlemail.com>
2019-10-19 15:16:25 +0200

    M1 Finished

--- Start.php
+++ Start.php
@@ -2,7 +2,7 @@
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Start</title>
     <!--
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     -->
@@ -18,7 +18,7 @@
         <div class="col-3" id="header_logo"><h1 id="logo_emensa">e-Mensa</h1></div>
         <nav class="col-6" id="header_menu">
             <ul class="nav" id="header_menu_nav">
-                <li class="nav-item"><a class="nav-link link_selected" href="Start.html">Start</a></li>
+                <li class="nav-item"><a class="nav-link link_selected" href="Start.php">Start</a></li>
                 <li class="nav-item"><a class="nav-link" href="Produkte.html">Mahlzeiten</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Bestellungen</a></li>
                 <li class="nav-item"><a class="nav-link" href="https://www.fh-aachen.de/" target="_blank">FH-Aachen</a></li>
@@ -34,7 +34,7 @@
     </header>
     <div class="row" id="content_top_row">
         <div class="col" id="content_top_row_picture">
-            <img class="img-fluid rounded" id="image_start_banner" src="img/food_banner_2_resized.jpg" title="Photo by Dan Gold on Unsplash">
+            <img class="img-fluid rounded" id="image_start_banner" src="img/food_banner_2_resized.jpg" title="Photo by Dan Gold on Unsplash" alt="IMAGE ERROR">
         </div>
     </div>
     <div class="row" id="content_bot_row">
@@ -43,7 +43,20 @@
             [ TEXT1  |
               TEXT2 ]|
             -->
+            <p class="text-hello">
+                <script>
+                    <?php
+                    if(isset($_GET['username']))
+                        echo "Guten Tag ", $_GET['username'];
+                    ?>
+                </script>
+            </p>
             <p class="text_info">
+                <script>
+                    <?php
+                    echo date("H:i",time());
+                    ?>
+                </script>
                 Der Dienst e-Mensa ist noch beta. Sie können bereits Mahlzeiten
                 durchstöbern, aber noch nicht bestellen.
             </p>
@@ -55,7 +68,7 @@
         <div class="col-9" id="content_bot_row_right">
             <!--
             |[HEADLINE]|[BUTTONS]
-            |[------PICTURE-----]
+            |[.....PICTURE......]
              -->
             <div class="row" id="content_bot_row_right_top">
                 <div class="col-9" id="content_bot_row_right_top_headline">
@@ -77,14 +90,20 @@
             </div>
             <div class="row" id="content_bot_row_right_bot">
                 <div class="col" id="content_bot_row_right_bot_picture">
-                    <img class="img-fluid rounded" id="image_start_banner_2" src="img/food_banner_2_resized.jpg" title="Photo by Dan Gold on Unsplash">
+                    <img class="img-fluid rounded" id="image_start_banner_2" src="img/food_banner_2_resized.jpg" title="Photo by Dan Gold on Unsplash" alt="IMAGE ERROR">
                 </div>
             </div>
         </div>
     </div>
     <footer class="row">
         <!--  Copyright | MENU |   -->
-        <div class="col-3" id="footer_copyright">&copy; 2019 Eugen Burikov</div>
+        <div class="col-3" id="footer_copyright">&copy;
+            <script>
+                <?php
+                echo date("d.n.Y",time());
+                ?>
+            </script>
+            Eugen Burikov</div>
         <nav class="col-6" id="footer_menu">
             <ul class="nav">
                 <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
